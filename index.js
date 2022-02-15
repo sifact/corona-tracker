94ddec1cc8b5d2c83945daa3fa3d993f27689436
Moshiur Sifat <58529765+sifact@users.noreply.github.com>
2022-02-15 06:00:32 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+export { default as Cards } from "./Cards/Cards";
+export { default as Chart } from "./Chart/Chart";
+export { default as CountryPicker } from "./CountryPicker/CountryPicker";
